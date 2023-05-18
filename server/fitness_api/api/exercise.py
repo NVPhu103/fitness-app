647fdc0f40cdf386520517ce575082edd4190a26
Phu <c.truong.phung@zonarsystems.com>
2023-05-18 12:32:50 +0700

    update get, get all, patch for exercise

--- server/fitness_api/api/exercise.py
+++ server/fitness_api/api/exercise.py
@@ -9,6 +9,7 @@ from fitness_api.schemas.exercise import (
     PatchExerciseModel,
     ExerciseModel,
 )
+from fitness_api.db.model import Exercise
 from fitness_api import ctrl
 from fitness_api.lib.paging import PaginationParams
 from fitness_api.lib.searcher.searcher import SearcherParams
@@ -31,3 +32,52 @@ async def create_exercise(
     exercise_record = await ctrl.exercise.create_exercise(post_exercise_model, session)
     exercise = ExerciseModel.from_orm(exercise_record).dict(by_alias=True)
     return exercise
+
+
+@router.get(
+    "", response_model=List[ExerciseModel], summary="Get all exercise", status_code=200
+)
+async def get_all_exercises(
+    request: Request,
+    response: Response,
+    searcher_params: SearcherParams = Depends(SearcherParams),
+    paging_params: PaginationParams = Depends(PaginationParams),
+    session: AsyncSession = Depends(create_session),
+) -> List[Exercise]:
+    content = await ctrl.exercise.list_all_exercises(
+        session, searcher_params, paging_params.page, paging_params.size
+    )
+    exercise_records: List[Exercise] = content.records
+    return exercise_records
+
+
+@router.get(
+    "/{exercise_id}", response_model=ExerciseModel, summary="Get a exercise", status_code=200
+)
+async def get_exercise(
+    request: Request,
+    response: Response,
+    exercise_id: UUID,
+    session: AsyncSession = Depends(create_session),
+) -> ExerciseModel:
+    exercise_record = await ctrl.exercise.get_exercise(session, exercise_id)
+    exercise = ExerciseModel.from_orm(exercise_record).dict(by_alias=True)
+    return exercise
+
+
+@router.patch(
+    "/{exercise_id}",
+    summary="Update a exercise",
+    status_code=204,
+    description="update the exercise by its ID",
+)
+async def update_food(
+    request: Request,
+    response: Response,
+    exercise_id: UUID,
+    patch_exercise_data: PatchExerciseModel,
+    session: AsyncSession = Depends(create_session),
+) -> None:
+    await ctrl.exercise.update_exercise(session, exercise_id, patch_exercise_data)
+    response.status_code = 204
+    return response
--- server/fitness_api/ctrl/exercise.py
+++ server/fitness_api/ctrl/exercise.py
@@ -31,3 +31,53 @@ async def create_exercise(
         raise HTTPException(
             detail="Exercise already exists", status_code=409
         ) from error
+
+
+
+async def list_all_exercises(
+    session: AsyncSession,
+    searcher_params: SearcherParams,
+    page: int = DEFAULT_PAGE_NUMBER,
+    size: int = DEFAULT_PAGE_SIZE,
+) -> PagedResultSet[Exercise]:
+    statement = select(Exercise)
+    searcher = Searcher(searcher_params, Exercise, ExerciseModel, "name")
+    statement = await searcher.apply_searcher(statement)
+    paged_statement = paginate_query(statement, page, size)
+
+    result = await session.execute(paged_statement)
+    exercise_records: List[Exercise] = result.scalars().all()
+    total_query = count_query(paged_statement, Exercise)
+    total = (await session.execute(total_query)).scalar_one()
+
+    return PagedResultSet(page=page, size=size, total=total, records=exercise_records)
+
+
+
+async def get_exercise(
+    session: AsyncSession,
+    exercise_id: UUID,
+) -> Optional[Exercise]:
+    exercise_record: Optional[Exercise] = await session.get(
+        Exercise, exercise_id, populate_existing=True
+    )
+    if exercise_record:
+        return exercise_record
+    else:
+        raise HTTPException(status_code=404, detail="Exercise does not exist")
+
+
+async def update_exercise(
+    session: AsyncSession,
+    exercise_id: UUID,
+    patch_exercise_data: PatchExerciseModel,
+) -> Exercise:
+    if not (data := patch_exercise_data.dict(exclude_unset=True)):
+        raise ValueError("No changes submitted.")
+    exercise_record = await get_exercise(session, exercise_id)
+    try:
+        exercise_record.apply_update(**data)
+        await session.commit()
+    except IntegrityError as exc:
+        await session.rollback()
+        raise HTTPException(status_code=400, detail="Error")
