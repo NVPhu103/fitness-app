6eec0968197ff4b1b2efd7ffcc71fa5573a39da8
Phu Nguyen <c.phu.nguyen@zonarsystems.com>
2023-12-13 23:28:01 +0700

    delete server

--- server/fitness_api/schemas/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class BaseModelWithConfig(BaseModel):
-    class Config:
-        orm_mode = True
-        allow_population_by_field_name = True
-        extra = "forbid"
